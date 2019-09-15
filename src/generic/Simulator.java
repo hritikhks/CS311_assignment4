011b9b12f4fe83940bd20e3293cefc60627ed5ac
luckynirania <lknmahan@gmail.com>
2019-09-16 01:42:04 +0530

    Loading of the program

--- src/generic/Simulator.java
+++ src/generic/Simulator.java
@@ -1,5 +1,11 @@
 package generic;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
 import processor.Clock;
 import processor.Processor;
 
@@ -8,7 +14,7 @@ public class Simulator {
 	static Processor processor;
 	static boolean simulationComplete;
 	
-	public static void setupSimulation(String assemblyProgramFile, Processor p)
+	public static void setupSimulation(String assemblyProgramFile, Processor p) throws FileNotFoundException
 	{
 		Simulator.processor = p;
 		loadProgram(assemblyProgramFile);
@@ -16,8 +22,34 @@ public class Simulator {
 		simulationComplete = false;
 	}
 	
-	static void loadProgram(String assemblyProgramFile)
+	static void loadProgram(String assemblyProgramFile) throws FileNotFoundException
 	{
+		System.out.println("inside load program "+assemblyProgramFile);
+		DataInputStream instr = new DataInputStream(new BufferedInputStream(new FileInputStream(assemblyProgramFile)));
+		try{
+			int n=instr.readInt();
+			int i;
+			for(i=0;i<n;i++){
+				int temp = instr.readInt();
+				processor.getMainMemory().setWord(i,temp);
+			}
+			int pc =i;
+			processor.getRegisterFile().setProgramCounter(pc);
+
+			while(instr.available()>0){
+				int temp = instr.readInt();
+				processor.getMainMemory().setWord(i,temp);
+				i++;
+			}
+			processor.getRegisterFile().setValue(0,0);
+			processor.getRegisterFile().setValue(1,65535);
+			processor.getRegisterFile().setValue(2,65535);
+
+			instr.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 		/*
 		 * TODO
 		 * 1. load the program into memory according to the program layout described
